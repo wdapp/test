1e43bcb10a86903a1004d5fa39072e346c3a79ff
wand <wand@bokecc.com>
2019-12-06 14:36:49 +0800

    xinzeng 555 xiugai
    xinzeng 555 xiugai

--- index/a.js
+++ index/a.js
@@ -6,6 +6,7 @@ var e = 11111111;
 var s = 555;
 var a = 111;
 var ccc = 111;
+console.log("xinzengneirong")
 console.log('修复bug')
 console.log("123")
 console.log('111')
@@ -15,3 +16,4 @@ console.log('222222')
 console.log('1111111')
 console.log('444')
 console.log('555')
+console.log('xinzeng')
