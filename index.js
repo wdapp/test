107c03c7b9531f02b9d6336e2c805653570b57e7
ada <11102069@qq.com>
2020-03-02 17:12:16 +0800

    22222222

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-var a = 5;
-console.log('hello')
-console.log('hello world'
-console.log("123")
-console.log("12321321")
-console.log("safsadfas")
-console.log(111)
-console.log(222)
-console.log(333)
-console.log(444)
-console.log(555)
-console.log(6666)
-console.log(777)
-console.log(888)
-console.log(999)
+console.log("index")
